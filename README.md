39397fdf38e23335c542fb53d5dd053f6f34ce12
kmille <github@androidloves.me>
2020-04-25 11:11:27 +0200

    Clean code and Update docs

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # memcat
 Print core memory of a given a process ID (linux specific)
+
+# How to use it
+sudo python3 memcat.py $(pidof nextcloud)
+
+# How does it work?
+- iterate over /proc/$pid/maps
+- open /proc/$pid/mem, seek(x), read(y)
+- dump it to a file
+- we need to ptrace to $pid so that we can read from /proc/$pid/mem
--- memcat.py
+++ memcat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Dump memory of a process (Linux only).
 # Based on Giles's answer from
@@ -6,18 +6,29 @@
 #
 # Error checking added by hackerb9.
 
-import ctypes, re, sys
+import ctypes
+import re
+import sys
+import os
+
+data_dir = "data"
 
 ## Partial interface to ptrace(2), only for PTRACE_ATTACH and PTRACE_DETACH.
 c_ptrace = ctypes.CDLL("libc.so.6").ptrace
 c_pid_t = ctypes.c_int32 # This assumes pid_t is int32_t
 c_ptrace.argtypes = [ctypes.c_int, c_pid_t, ctypes.c_void_p, ctypes.c_void_p]
+
+
 def ptrace(attach, pid):
     op = ctypes.c_int(16 if attach else 17) #PTRACE_ATTACH or PTRACE_DETACH
     c_pid = c_pid_t(pid)
     null = ctypes.c_void_p()
     err = c_ptrace(op, c_pid, null, null)
-    if err != 0: raise (SysError, 'ptrace', err)
+    if err != 0:
+        print(f"Error calling ptrace: {err}")
+        print("Call it with sudo?")
+        print("Or change ptrace_scope: https://wiki.archlinux.org/index.php/Security#ptrace_scope")
+        sys.exit(1)
 
 ## Parse a line in /proc/$pid/maps. Return the boundaries of the chunk
 ## the read permission character.
@@ -29,14 +40,15 @@ def maps_line_range(line):
 def cat_proc_mem(pid):
     ## Apparently we need to ptrace(PTRACE_ATTACH, $pid) to read /proc/$pid/mem
     ptrace(True, int(pid))
-    mem_file=None
+    mem_file = None
     try:
-        ## Read the memory maps to see what address ranges are readable
-        maps_file = open("/proc/" + pid + "/maps", 'r')
+        # Read the memory maps to see what address ranges are readable
+        maps_file = open(f"/proc/{pid}/maps", 'r')
         ranges = map(maps_line_range, maps_file.readlines())
         maps_file.close()
-        ## Read the readable mapped ranges
-        mem_file = open("/proc/" + pid + "/mem", 'rb', 0)
+
+        # Read the readable mapped ranges
+        mem_file = open(f"/proc/{pid}/mem", 'rb', 0)
         for r in ranges:
             if r[2] == 'r':
                 try:
@@ -51,12 +63,25 @@ def cat_proc_mem(pid):
                     # Some sections may not be readable, e.g., /dev/dri/card0
                     sys.stderr.write("Warning, cannot read %X - %X%s: %s\n" % (r[0],r[1], " (%s)" % (r[3]) if r[3] else "", e))
                     pass
-                sys.stdout.buffer.write(chunk)
-    ## Cleanup
+
+                filename = r[-1].replace("/", "") if r[-1] else "else"
+                with open(f"{data_dir}/{filename}.mem", "ab") as f:
+                    print(f"Dumping memory to {data_dir}/{filename}.mem")
+                    f.write(chunk)
+                #sys.stdout.buffer.write(chunk)
+    # Cleanup
     finally:
-        if mem_file:  mem_file.close()
+        if mem_file:
+            mem_file.close()
         ptrace(False, int(pid))
 
+
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(f"Need at least one process id. python3 {sys.argv[0]} <pid>")
+        sys.exit(1)
+
     for pid in sys.argv[1:]:
         cat_proc_mem(pid)
+    print(f"Done. Check the files in {data_dir}")
+
